f4063eb7c5095715bedf9ace0ae248ba60051313
namvietquat <77259693+namvietquat@users.noreply.github.com>
2025-09-18 21:18:22 +0700

    Add Input and Action Jump, Move

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,24 +1,76 @@
 using System;
 using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _rb;
-    public float Speed = 3;
+    public float Speed = 3f;
+    public float JumpForce = 3f;
+    private bool _isOnAir;
+    private float _horizontalInput;
+    public LayerMask GroundLayerMask;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        if (_rb == null)
+        {
+            Debug.LogError("Rigidbody2D chua duoc gan vao Player!");
+        }
     }
-
+    void Update()
+    {
+        
+    
+    }
     void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        _rb.linearVelocity = new Vector2(horizontalInput * Speed, _rb.linearVelocity.y);
+        _horizontalInput = Input.GetAxis("Horizontal");
+        Flip(_horizontalInput);
+        GroundCheck();
+
+        // Di chuyen ngang
+        _rb.linearVelocity = new Vector2(_horizontalInput * Speed, _rb.linearVelocity.y);
+    }
+    public void Move(CallbackContext ctx)
+    {
+        Vector2 movement = ctx.ReadValue<Vector2>();
+        
+        _horizontalInput = movement.x;
+
+    }
+    public void Jump(CallbackContext ctx)
+    {
+        if (_isOnAir) return;
+
+        
+        float jumpInput = ctx.ReadValue<float>();
+
+        _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpInput * JumpForce);
+        
+        _isOnAir = true;
+    }
+
+    void Flip(float direction)
+    {
+        if (direction == 0) return;
+
+        if ((direction > 0) != (transform.localScale.x > 0))
+        {
+            transform.localScale *= new Vector2(-1f, 1f);
+        }
+    }
+
+    void GroundCheck()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.2f, GroundLayerMask);
+        Debug.DrawRay(transform.position, Vector2.down * 1.2f, Color.red);
+        _isOnAir = hit.collider == null;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("va cham");
+        Debug.Log("Va cham voi: " + collision.name);
     }
 }
